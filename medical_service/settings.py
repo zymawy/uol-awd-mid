debf2ca1b40f3b65f98a0bf4181b4fa0f4990e8b
zymawy <zymawy@gamil.com>
2024-06-17 04:32:20 +0300

    reslove digital issue
    # Why is this change being made?
    
    # Provide links to any relevant tickets, URLs or other resources

--- medical_service/settings.py
+++ medical_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,5 @@ REST_FRAMEWORK = {
 	'EXCEPTION_HANDLER': 'medicalapp.exceptions.custom_exception_handler',
 	'PAGE_SIZE': 15,
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
